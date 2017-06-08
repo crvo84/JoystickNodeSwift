8c7f5eb5804c6af728fd9738dd4db5bc1cec119e
crvo84 <crvo84@gmail.com>
2017-06-08 00:59:04 -0500

    Implement KAZViewController

---
+++ JoystickNode/KAZViewController.swift
@@ -0,0 +1,57 @@
+//
+//  KAZViewController.swift
+//  JoystickNode
+//
+//  Created by Carlos Villanueva Ousset on 6/8/17.
+//  Copyright © 2017 Villou. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class KAZViewController: UIViewController {
+    
+    override var shouldAutorotate: Bool {
+        return false
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if let skView = view as? SKView {
+            // Configure view
+            skView.showsFPS = true
+            skView.showsNodeCount = true
+            
+            // Create and configure scene
+            let scene = KAZDynamicControllerScene(size: skView.bounds.size)
+            scene.scaleMode = .aspectFill
+            
+            // Present scene
+            skView.presentScene(scene)
+        }
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .all
+        }
+    }
+    
+    @IBAction func changeScene(_ sender: UISegmentedControl) {
+        guard let skView = view as? SKView else { return }
+        
+        let scene: SKScene
+        if sender.selectedSegmentIndex == 0 {
+            scene = KAZDynamicControllerScene(size: skView.bounds.size)
+        } else {
+            // TODO: present fixed controller scene
+            scene = KAZDynamicControllerScene(size: skView.bounds.size)
+        }
+        scene.scaleMode = .aspectFill
+        skView.presentScene(scene)
+    }
+    
+}
